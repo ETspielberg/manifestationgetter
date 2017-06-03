56dd5cd7421681143e469a9ef3387b05324d05d6
Eike Spielberg <Eike.Spielberg@uni-due.de>
2017-06-03 08:38:23 +0200

    initial commit

---
+++ src/main/java/unidue/ub/services/manifestgetter/ManifestGetterApplication.java
@@ -0,0 +1,12 @@
+package unidue.ub.services.manifestgetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManifestGetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManifestGetterApplication.class, args);
+	}
+}
