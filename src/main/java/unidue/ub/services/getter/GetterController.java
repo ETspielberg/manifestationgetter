f699c991c340791fd8286ec069c9d2c83a7e79d4
Eike Spielberg <Eike.Spielberg@uni-due.de>
2018-09-19 12:58:19 +0200

    refactoring to reduce lines of code, changing the way several arguments are read into a list of shelfmarks.

--- src/main/java/unidue/ub/services/getter/GetterController.java
+++ src/main/java/unidue/ub/services/getter/GetterController.java
@@ -1,9 +1,5 @@
 package unidue.ub.services.getter;
 
-import java.sql.SQLException;
-import java.util.*;
-import java.util.regex.Pattern;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +10,12 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import unidue.ub.media.monographs.Item;
 import unidue.ub.media.monographs.Manifestation;
+
+import java.sql.SQLException;
+import java.util.*;
+import java.util.regex.Pattern;
 
 @Controller
 @RefreshScope
@@ -88,7 +87,7 @@ public class GetterController implements GetterClient {
         shelfmarks = new HashSet<>();
 
         Set<String> shelfmarksQueried = new HashSet<>();
-        Set<String> itemIds = new HashSet<>();
+
         Set<Manifestation> manifestations = new HashSet<>();
         Set<String> manifestationsQueried = new HashSet<>();
         log.info("queried identifier: " + identifier);
@@ -96,12 +95,8 @@ public class GetterController implements GetterClient {
         Boolean exactBoolean = "true".equals(exact);
         boolean shelfmarkNew;
 
-
         ManifestationGetter manifestationgetter = new ManifestationGetter(jdbcTemplate);
         manifestationgetter.setShelfmarkRegex(shelfmarkRegex);
-        ItemGetter itemGetter = new ItemGetter(jdbcTemplate);
-        EventGetter eventgetter = new EventGetter(jdbcTemplate);
-        MABGetter mabGetter = new MABGetter(jdbcTemplate);
 
         Pattern pattern = Pattern.compile(shelfmarkRegex);
 
@@ -109,13 +104,14 @@ public class GetterController implements GetterClient {
             shelfmarks.addAll(manifestationgetter.getShelfmarkFromBarcode(identifier));
         else {
             identifier = deleteItemIdentifier(identifier);
-            if (identifier.contains("; ")) {
-                shelfmarks.addAll(Arrays.asList(identifier.split("; ")));
+            if (identifier.contains(";")) {
+                String[] individualIdentifiers = identifier.split(";");
+                for (String individualIdentifier: individualIdentifiers)
+                    if (!individualIdentifier.isEmpty())
+                        shelfmarks.add(individualIdentifier);
             } else
-                shelfmarks.add(identifier);
+                shelfmarks.add(identifier.trim());
         }
-
-
         do {
             for (String shelfmark : shelfmarks) {
                 log.info("collecting shelfmark " + shelfmark);
@@ -130,18 +126,8 @@ public class GetterController implements GetterClient {
                 for (Manifestation foundManifestation : foundManifestations) {
                     if (manifestationsQueried.contains(foundManifestation.getTitleID()))
                         continue;
-                    List<Item> items = itemGetter.getItemsByDocNumber(foundManifestation.getTitleID());
-                    for (Item item : items) {
-                        if (itemIds.contains(item.getItemId()))
-                            continue;
-                        if (ignoredCollections.contains(item.getCollection()))
-                            continue;
-                        foundManifestation.addItem(item);
-                        itemIds.add(item.getItemId());
-                    }
-                    eventgetter.addEventsToManifestation(foundManifestation);
+                    extendManifestation(foundManifestation, false);
                     manifestations.add(foundManifestation);
-
                     if (!manifestationsQueried.contains(foundManifestation.getTitleID()))
                         manifestationsQueried.add(foundManifestation.getTitleID());
                 }
@@ -153,20 +139,14 @@ public class GetterController implements GetterClient {
                     shelfmarksInManifestations.addAll(Arrays.asList(manifestation.getShelfmarks()));
                 }
                 for (String callNo : shelfmarksInManifestations) {
-                        callNo = deleteItemIdentifier(callNo);
-                        callNo = getReferenceShelfmark(callNo);
-                        shelfmarkNew = (shelfmarkNew || isShelfmarkNew(callNo)) && pattern.matcher(callNo).find();
-                        if (shelfmarkNew)
-                            shelfmarks.add(callNo);
-                    }
+                    callNo = deleteItemIdentifier(callNo);
+                    callNo = getReferenceShelfmark(callNo);
+                    shelfmarkNew = (shelfmarkNew || isShelfmarkNew(callNo)) && pattern.matcher(callNo).find();
+                    if (shelfmarkNew)
+                        shelfmarks.add(callNo);
                 }
+            }
         } while (shelfmarkNew);
-
-        for (Manifestation manifestation : manifestations) {
-            StockEventsBuilder.buildStockEvents(manifestation);
-            mabGetter.addSimpleMAB(manifestation);
-            manifestation.buildUsergroupList();
-        }
         List<Manifestation> manifestationsList = new ArrayList<>(manifestations);
         Collections.sort(manifestationsList);
         return ResponseEntity.ok(manifestationsList);
@@ -189,15 +169,15 @@ public class GetterController implements GetterClient {
     @GetMapping("/buildFullManifestation")
     public ResponseEntity<?> buildFullManifestation(@RequestParam("identifier") String identifier) {
         Manifestation manifestation = new Manifestation(identifier);
-        extendManifestation(manifestation);
+        extendManifestation(manifestation, false);
         return ResponseEntity.ok(manifestation);
     }
-    
+
     @Override
     @GetMapping("buildActiveManifestation")
     public ResponseEntity<?> buildActiveManifestation(@RequestParam("identifier") String identifier) {
         Manifestation manifestation = new Manifestation(identifier);
-        extendActiveManifestation(manifestation);
+        extendManifestation(manifestation, true);
         return ResponseEntity.ok(manifestation);
     }
 
@@ -206,46 +186,38 @@ public class GetterController implements GetterClient {
     public void getJournalPrices(@RequestParam("identifier") String identifier, @RequestParam("type") String type) throws SQLException {
         JournalPriceGetter getter = new JournalPriceGetter(jdbcTemplate);
         switch (type) {
-            case "journalcollection" :  {
+            case "journalcollection": {
                 getter.getJournalcollectionprices(identifier);
                 break;
             }
-            case "journal" :  {
+            case "journal": {
                 getter.getJournalPrice(identifier);
                 break;
             }
         }
     }
 
-    private void extendActiveManifestation(Manifestation manifestation) {
+    private void extendManifestation(Manifestation manifestation, Boolean active) {
         Set<String> itemIds = new HashSet<>();
         ItemGetter itemGetter = new ItemGetter(jdbcTemplate);
         EventGetter eventgetter = new EventGetter(jdbcTemplate);
         MABGetter mabGetter = new MABGetter(jdbcTemplate);
         List<Item> items = itemGetter.getItemsByDocNumber(manifestation.getTitleID());
-        for (Item item : items)
-            if (!itemIds.contains(item.getItemId())) {
-                manifestation.addItem(item);
-                itemIds.add(item.getItemId());
-            }
-        eventgetter.addAcitveEventsToManifestation(manifestation);
+        for (Item item : items) {
+            if (itemIds.contains(item.getItemId()))
+                continue;
+            if (ignoredCollections.contains(item.getCollection()))
+                continue;
+            manifestation.addItem(item);
+            itemIds.add(item.getItemId());
+        }
+        if (active) {
+            eventgetter.addAcitveEventsToManifestation(manifestation);
+        } else {
+            eventgetter.addEventsToManifestation(manifestation);
+            StockEventsBuilder.buildStockEvents(manifestation);
+            manifestation.buildUsergroupList();
+        }
         mabGetter.addSimpleMAB(manifestation);
-    }
-
-    private void extendManifestation(Manifestation manifestation) {
-        Set<String> itemIds = new HashSet<>();
-        ItemGetter itemGetter = new ItemGetter(jdbcTemplate);
-        EventGetter eventgetter = new EventGetter(jdbcTemplate);
-        MABGetter mabGetter = new MABGetter(jdbcTemplate);
-        List<Item> items = itemGetter.getItemsByDocNumber(manifestation.getTitleID());
-        for (Item item : items)
-            if (!itemIds.contains(item.getItemId())) {
-                manifestation.addItem(item);
-                itemIds.add(item.getItemId());
-            }
-        eventgetter.addEventsToManifestation(manifestation);
-        StockEventsBuilder.buildStockEvents(manifestation);
-        mabGetter.addSimpleMAB(manifestation);
-        manifestation.buildUsergroupList();
     }
 }
